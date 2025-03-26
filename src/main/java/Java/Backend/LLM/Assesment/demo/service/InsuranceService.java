074d9c55d3b412b27301fddb1feb3b18079ec365
shobhit15082003 <shobhitbtech1508@gmail.com>
2025-03-26 19:27:55 +0530

    service changes

--- src/main/java/Java/Backend/LLM/Assesment/demo/service/InsuranceService.java
+++ src/main/java/Java/Backend/LLM/Assesment/demo/service/InsuranceService.java
@@ -1,4 +1,12 @@
 package Java.Backend.LLM.Assesment.demo.service;
 
+import Java.Backend.LLM.Assesment.demo.model.Insurance;
+
+import java.util.List;
+
 public interface InsuranceService {
+
+    List<Insurance> getAllInsurance();
+    Insurance purchaseInsurance(Insurance insurance);
+    String downloadInsurance(Integer age,String gender, Double income);
 }
--- src/main/java/Java/Backend/LLM/Assesment/demo/service/InsuranceServiceImpl.java
+++ src/main/java/Java/Backend/LLM/Assesment/demo/service/InsuranceServiceImpl.java
@@ -1,4 +1,32 @@
 package Java.Backend.LLM.Assesment.demo.service;
 
-public class InsuranceServiceImpl {
+import Java.Backend.LLM.Assesment.demo.model.Insurance;
+import Java.Backend.LLM.Assesment.demo.repository.InsuranceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InsuranceServiceImpl implements InsuranceService{
+        private final InsuranceRepository insuranceRepository;
+
+    public InsuranceServiceImpl(InsuranceRepository insuranceRepository) {
+        this.insuranceRepository = insuranceRepository;
+    }
+
+    @Override
+    public List<Insurance> getAllInsurance(){
+        return insuranceRepository.findAll();
+    }
+
+    @Override
+    public Insurance purchaseInsurance(Insurance insurance) {
+        return insuranceRepository.save(insurance);
+    }
+
+    @Override
+    public String downloadInsurance(Integer age, String gender, Double income) {
+        return "";
+    }
+
 }
