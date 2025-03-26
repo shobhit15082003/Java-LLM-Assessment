d809b1ed0bf85220d6247ea0d1ad6de3a17d35e5
shobhit15082003 <shobhitbtech1508@gmail.com>
2025-03-26 19:02:22 +0530

    models done

--- src/main/java/Java/Backend/LLM/Assesment/demo/model/Insurance.java
+++ src/main/java/Java/Backend/LLM/Assesment/demo/model/Insurance.java
@@ -1,4 +1,30 @@
 package Java.Backend.LLM.Assesment.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String policyHolderName;
+    private String policyType;
+    private double coverageAmount;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
 }
--- src/main/java/Java/Backend/LLM/Assesment/demo/repository/InsuranceRepository.java
+++ src/main/java/Java/Backend/LLM/Assesment/demo/repository/InsuranceRepository.java
@@ -1,4 +1,10 @@
 package Java.Backend.LLM.Assesment.demo.repository;
 
-public class InsuranceRepository {
+import Java.Backend.LLM.Assesment.demo.model.Insurance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InsuranceRepository extends JpaRepository<Insurance,Long> {
+
 }
